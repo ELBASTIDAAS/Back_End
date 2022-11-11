626f682e612296b1c4ddc6166a0ba98ba4c196cb
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-11-10 19:23:48 -0800

    .

---
+++ python/myFistPythonScript.py
@@ -0,0 +1,13 @@
+name = "Frank"
+age = 80
+if age < 50:
+    print ("You are young")
+elif age == 50:
+    print ("You are old")
+else:
+    print ("Yes youre old, but dont worry")
+
+if name == "Saul":
+    print ("Hello there " + name)
+else:
+    print ("You are not " + name)
