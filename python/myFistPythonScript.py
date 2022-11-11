d8790d939c44dfbcaf9239f8ec869d13e1613a1e
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-11-10 19:47:07 -0800

    .

--- python/myFistPythonScript.py
+++ python/myFistPythonScript.py
@@ -1,13 +1,19 @@
 name = "Frank"
 age = 80
 if age < 50:
-    print ("You are young")
+    print("You are young")
 elif age == 50:
-    print ("You are old")
+    print("You are old")
 else:
-    print ("Yes youre old, but dont worry")
+    print("Yes youre old, but dont worry")
 
 if name == "Saul":
-    print ("Hello there " + name)
+    print("Hello there " + name)
 else:
-    print ("You are not " + name)
+    print("You are not " + name)
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+totalOfSum = 0
+for num in nums:
+    totalOfSum = totalOfSum + num
+    print(totalOfSum)
